feb1f74e71dab7d63c0b485998462f3bbc6e1313
bittuurlwebwala <bittuurl.webwala@gmail.com>
2026-06-04 13:46:18 +0530

    fix: mongodb connection + vercel setup

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-require("dotenv").config();
+
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
+require("dotenv").config();
+
 const PORT = process.env.PORT || 5000;
+console.log(process.env.PORT)
+
 
 // Connect to Database and then start server
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
+
+
